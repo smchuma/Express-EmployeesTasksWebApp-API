8519e7616f640077b5776030fad53a29149520e7
Samora Laurent <laurentsamora6@gmail.com>
2022-10-20 16:51:43 +0300

    created logger.js to handle log requests

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { logger } = require("./middleware/logger");
 const PORT = process.env.PORT || 4000;
 
+app.use(logger);
+
+app.use(express.json());
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/", require("./routes/root"));
 
