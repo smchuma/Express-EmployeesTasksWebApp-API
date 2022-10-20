0b457309f0e3e18fc6638a8a9b8143126c8de194
Samora Laurent <laurentsamora6@gmail.com>
2022-10-20 15:10:06 +0300

    create the 404 page

--- server.js
+++ server.js
@@ -6,4 +6,16 @@ const PORT = process.env.PORT || 4000;
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/", require("./routes/root"));
 
+app.all("*", (req, res) => {
+  res.status(404);
+
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
+});
+
 app.listen(PORT, () => console.log("Listening on port " + PORT));
